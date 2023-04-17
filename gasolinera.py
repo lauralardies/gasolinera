02d2442913d6a02e060d51afa2eebc3472f05287
lauralardies <lrodrlar@myuax.com>
2023-04-17 16:59:38 +0200

    intervalo y suritdores

---
+++ gasolinera.py
@@ -0,0 +1,6 @@
+import random
+
+intervalo =  int(input('¿Cuál es el intervalo máximo en el que llegan los coches? >> '))
+intervalo = random.randint(0, intervalo) # Elegimos un intervalo aleatorio
+surtidores = int(input('¿Con cuántos surtidores cuenta la gasolinera? >> '))
+
