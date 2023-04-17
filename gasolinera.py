ea3e5bec7a67c376f385f0098e16df48b7bd4bdc
lauralardies <lrodrlar@myuax.com>
2023-04-17 17:21:52 +0200

    avances

--- gasolinera.py
+++ gasolinera.py
@@ -1,6 +1,13 @@
 import random
+from surtidor import Surtidor
 
-intervalo =  int(input('¿Cuál es el intervalo máximo en el que llegan los coches? >> '))
-intervalo = random.randint(0, intervalo) # Elegimos un intervalo aleatorio
-surtidores = int(input('¿Con cuántos surtidores cuenta la gasolinera? >> '))
+surtidor = Surtidor()
+
+while True:
+    t = random.randint(0, 15) # Se escoge aleatoriamente el tiempo en el que viene el siguiente coche
+    for i in range(16): # Pasa el tiempo por cada vez que se ejecuta bucle for
+        if i == t: # Sólo entramos si ha pasado el tiempo en el que llega el siguiente coche
+            print('Entra un coche a la gasolinera')
+
+            
 
---
+++ surtidor.py
@@ -0,0 +1,16 @@
+class Surtidor():
+    def __init__(self) -> None:
+        self.ocupado = False # Comienza el surtidor desocupado
+        self.cola = [] # Como comienza desocupado, la cola está vacía --> NO se cuenta la persona que está echando la gasolina
+    
+    def ocupado(self):
+        '''
+        Función que muestra si el surtidor está ocupado
+        '''
+        return self.ocupado
+    
+    def largo_cola(self):
+        '''
+        Función que muestra cómo de larga es la cola del surtidor
+        '''
+        return len(self.cola)
